f76621c2f336adef4a302b6ce0b261f92c034f2b
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-26 21:48:04 +0100

    Start of questions for products.js

--- js/products.js
+++ js/products.js
@@ -14,3 +14,22 @@ const products = [
         soldOut: true,
     },
 ];
+
+
+const showProducts = document.querySelector(".container");
+console.log(showProducts);
+
+let html = "";
+
+for (let i = 0; i < products.length; i++) {
+    html = html + `
+    <h2> Name: ${products[i].name} </h2>
+    <p>Price: ${products[i].price}</p>
+    <p>In stock: ${products[i].soldOut}</p>
+    `;
+
+    console.log(html);
+}
+
+showProducts.innerHTML = html;
+
